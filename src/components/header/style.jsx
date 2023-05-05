54004718b2f4af6ac3638bd82024192556e84a7d
-v <gustavoleck@hotmail.com>
2023-05-05 00:12:18 -0300

    build(Criado componentes e logicas para consulta de filmes)

---
+++ src/components/header/style.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #213635;
+  height: 200px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 8px 2px #348e91;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+`;
+
+export const Imagem = styled.img`
+  height: 80%;
+  margin-top: 1%;
+`;
