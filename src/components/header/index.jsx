d4ae62469cdca47086e379a0f205c3145b888c59
-v <gustavoleck@hotmail.com>
2023-06-03 19:51:14 -0300

    feat(Reafatorado formulario e criado seção de filme pesquisado):

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,12 @@
 import { Container, Imagem } from "./style";
 import Logo from "../../assets/saturday-logo-white.png";
+import Formulario from "./../formulario";
 
 export default function Header() {
   return (
     <Container>
       <Imagem src={Logo} />
+      <Formulario></Formulario>
     </Container>
   );
 }
