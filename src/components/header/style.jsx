e7af7f386c08a2060c076e8b487318ccfdcbbc36
-v <gustavoleck@hotmail.com>
2023-06-03 19:51:19 -0300

    feat(Reafatorado formulario e criado seção de filme pesquisado):

--- src/components/header/style.jsx
+++ src/components/header/style.jsx
@@ -1,16 +1,18 @@
-import styled from "styled-components";
+import style from "styled-components";
 
-export const Container = styled.div`
-  background-color: #213635;
+export const Container = style.div`
+  background-color: #4c007d;
   height: 200px;
+
   border-radius: 20px;
-  box-shadow: 0px 0px 8px 2px #348e91;
+  box-shadow: 0px 0px 8px 2px #fefefe;
   display: flex;
   justify-content: center;
   align-content: center;
 `;
 
-export const Imagem = styled.img`
+export const Imagem = style.img`
   height: 80%;
+  margin-left:0px;
   margin-top: 1%;
 `;
